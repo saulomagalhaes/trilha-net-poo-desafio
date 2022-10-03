54fbf0911a5c8e1a29dd09b3b21d3bd0ab31172c
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-03 14:57:37 -0300

    feat: :sparkles: finalizando o desafio de poo da trilha pottencial .NET developer com as classes implementadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("11 99999-9999", "Nokia 3310", "123456789", 32);
+nokia.InstalarAplicativo("WhatsApp");
+nokia.Ligar();
+nokia.ReceberLigacao();
+Smartphone iphone = new Iphone("11 99999-9999", "iPhone 11", "123456789", 64);
+iphone.InstalarAplicativo("WhatsApp");
+iphone.Ligar();
+iphone.ReceberLigacao();
